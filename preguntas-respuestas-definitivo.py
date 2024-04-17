8d81cc736b173a382b8ebd5b7c32192acb6d700c
silviapazos <silvia@LAPTOP-N355F8IA.localdomain>
2024-04-17 18:57:42 +0200

    añadidos emoticonos y espacios

--- preguntas-respuestas-definitivo.py
+++ preguntas-respuestas-definitivo.py
@@ -1,30 +1,29 @@
 #JUEGO PREGUNTAS Y RESPUESTAS
 # Definimos las preguntas y respuestas con un diccionario
 
-preguntas_geografía={"¿Cuál es la montaña más alta de España? (Escríbelo sin artículos)": "Teide",
-                    "¿El mar muerto es realmente un mar? (Escribe 'sí' o 'no')":"No",
-                    "¿Qué continente es el más poblado?":"Asia",
-                    "¿Qué río pasa por más países? (Escríbelo sin artículos)":"Danubio",
-                    "¿Cuál es el país más pequeño del mundo? (Escríbelo sin artículos)":"Ciudad del Vaticano",
-                    "¿Cuántos océanos hay en la Tierra? (Inserta un número)" : "5",
-                    "¿Qué río pasa por París? (Escríbelo sin artículos)" : "Sena",
-                    "¿Cuál es el río más largo de España? (Escríbelo sin artículos)" : "Tajo",
-                    "¿Qué ocupa más superficie, el agua o los continentes? (Escribe 'agua' o 'continentes')" : "agua",
-                    "¿Cuál es el continente más seco del planeta? (Escribe sin acentos ni artículos)": "Antartida",
-                    "¿Cuál es la isla más grande del mundo?" : "Groenlandia",
-                    "¿Cual es la capital de Argentina?" : "Buenos Aires"}
+preguntas_geografía={"¿Cuál es la montaña más alta de España? (Escríbelo sin artículos)\n": "Teide",
+                    "¿El mar muerto es realmente un mar? (Escribe 'sí' o 'no')\n":"No",
+                    "¿Qué continente es el más poblado?\n":"Asia",
+                    "¿Qué río pasa por más países? (Escríbelo sin artículos)\n":"Danubio",
+                    "¿Cuál es el país más pequeño del mundo? (Escríbelo sin artículos)\n":"Ciudad del Vaticano",
+                    "¿Cuántos océanos hay en la Tierra? (Inserta un número)\n" : "5",
+                    "¿Qué río pasa por París? (Escríbelo sin artículos)\n" : "Sena",
+                    "¿Cuál es el río más largo de España? (Escríbelo sin artículos)\n" : "Tajo",
+                    "¿Qué ocupa más superficie, el agua o los continentes? (Escribe 'agua' o 'continentes')\n" : "agua",
+                    "¿Cuál es el continente más seco del planeta? (Escribe sin acentos ni artículos)\n": "Antartida",
+                    "¿Cuál es la isla más grande del mundo?\n" : "Groenlandia",
+                    "¿Cual es la capital de Argentina?\n" : "Buenos Aires"}
 
 #Importamos la librería random para que podamos dar resultados aleatorios más adelante:
 import random
 
 # Iniciamos juego dando la bienvenida y explicando las reglas del juego.
-print("WELCOME! En este juego de preguntas y respuestas de geografía, pondrás a prueba tus conocimientos. 🌍 \n\n ¡Piensa bien cada respuesta! La partida terminará cuando alcances 3 fallos. \n Para ganar, deberás acertar 5 preguntas. \n Fácil, ¿no? \n")
-#print("*"*60)
+print("\n                           🕌🏔️🌍 WELCOME! 🌍🏔️🕌 \n\n En este juego de preguntas y respuestas de geografía, pondrás a prueba tus conocimientos. \n\n ¡Piensa bien cada respuesta! La partida terminará cuando alcances 3 fallos. \n Para ganar, deberás acertar 5 preguntas. \n Fácil, ¿no? \n")
 print("🍀🍀🍀🍀🍀🍀🍀🍀🍀🍀🍀\n")
 print("¿Estás preparado/a?")
 start = input("Pulsa cualquier letra para empezar a jugar.")
 pass
-print("START \n")
+print("START\n")
 print("🍀🍀🍀🍀🍀🍀🍀🍀🍀🍀🍀\n")
 
 # Creamos una funcion para seleccionar y ofrecer a la persona usuaria una pregunta aleatoria de nuestro diccionario, sin que esta se repita.
@@ -53,26 +52,26 @@ def juego():
                     print("¡Has acertado! \n")
                     respuestas_correctas += 1
                     print(f"LLevas {respuestas_correctas} aciertos.🤓")
-                    print("<3"*40)
+                    print("*"*60)
                     
 
                     if respuestas_correctas == 5:
                         print("\n")
                         print("*"*60)
-                        print("ENHORABUENA \n ¡¡HAS GANADO EL JUEGO!! 🥳🧠🤟 \n\n\n")
+                        print("                💥¡¡ENHORABUENA!!💥 \n       🥳🧠🤟¡¡HAS GANADO EL JUEGO!!🥳🧠🤟\n\n\n") 
                         break
 
                 else:
                     fallos += 1
                     print(f"Tu respuesta es {respuesta_usuario}")
                     print(f"¡Ups! Has fallado. La respuesta correcta es {respuestas}.\n") 
-                    print(f"¡Cuidado!😱 Llevas {fallos} fallos.")
-                    print("-"*60) 
+                    print(f"¡Cuidado! 😱 Llevas {fallos} fallos.")
+                    print("*"*60) 
 
                     if fallos==3:
                         print("\n")
-                        print("*"*60)
-                        print("¡GAME OVER! 🚨🚨🚨\n HAS FALLADO 3 VECES.\n\n\n\n")
+                        #print("*"*60)
+                        print(" 🚨🚨🚨🚨🚨🚨 ¡GAME OVER! 🚨🚨🚨🚨🚨🚨 \n\n             HAS FALLADO 3 VECES.\n\n\n\n")
                         break
 
             else:
